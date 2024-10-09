d37bdfbac9b9ee6e1988bc134525b9e89e9b90d6
Alex Hsu <55427893+yhhsu@users.noreply.github.com>
2024-10-09 21:55:30 +0800

    Update modelisation.py

--- modelisation.py
+++ modelisation.py
@@ -6,6 +6,8 @@ import seaborn as sns
 import streamlit_option_menu
 from streamlit_option_menu import option_menu
 
+df = pd.read_csv("data_preprocessed.csv")
+
 def app():
     st.title("Modélisation")
 
