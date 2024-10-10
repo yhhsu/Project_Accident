d531f61af5ade831fcadb6ceb4ac3b7d4a14dbee
Alex Hsu <55427893+yhhsu@users.noreply.github.com>
2024-10-10 22:03:37 +0800

    Update conclusion.py

--- conclusion.py
+++ conclusion.py
@@ -8,6 +8,7 @@ from streamlit_option_menu import option_menu
 
 def app():
     st.title("Conclusion")
+    st.write("### Difficultés rencontrées lors du projet")
     st.write('''
     Nous avons rencontré deux principaux verrous scientifiques. Le premier était 
     l’absence de corrélation significative entre les variables individuelles et la variable 
@@ -18,4 +19,13 @@ def app():
     investir  un  temps  considérable  dans  l’optimisation  de  ce  modèle,  malgré  des 
     résultats initialement décevants. Cette focalisation excessive sur XGBoost a retardé 
     l’exploration d’autres modèles potentiellement plus adaptés à notre problématique.
+    ''')
+    st.write('''
+    Ainsi nous avons consacré une quantité considérable de temps à l’instanciation des 
+    modèles,  et  ces  derniers  nécessitaient  des  durées  d’exécution  extrêmement 
+    prolongées en raison de la volumétrie importante du jeu de données. Cette situation 
+    a considérablement ralenti notre progression et a exigé des ressources 
+    computationnelles substantielles.  De plus, d’un point  de vue IT, la puissance  de 
+    stockage et la capacité computationnelle des ordinateurs de certains membres de 
+    l’équipe n’étaient pas adaptées, ce qui a encore accentué les défis rencontrés.
     ''')
