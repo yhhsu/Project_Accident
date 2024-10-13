64429e5642f401898cf4965898e9b43c708235ee
cedrickhonekhammy <41993081+cedrickhonekhammy@users.noreply.github.com>
2024-10-13 16:54:34 +0200

    Update modelisation.py

--- modelisation.py
+++ modelisation.py
@@ -37,8 +37,6 @@ def app():
         3: 2, # Hospitalisé
         4: 3 # Léger
     }
-
-    st.write("Les colonnes du DataFrame sont :", df.columns.tolist())
     
     # Application du mapping
     df['grav'] = df['grav'].map(labels)
@@ -72,6 +70,7 @@ def app():
     #model_rf_sfm_2_best = joblib.load('models/model_rf_sfm_2_best.joblib')
     
     # Partie présentation des résultats pour différents types de modèles
+    # Partie 1
     data1 = {
     'Modèle': ['Xgboost brut', 'Random forest brut', 'Lightgbm brut'],
     'Accuracy': [0.60 , 0.63, 0.59],
@@ -82,9 +81,50 @@ def app():
     df1 = pd.DataFrame(data1)
     df1 = df1.set_index('Modèle')
 
-    st.write("## Comparaison de différents type de modèles bruts")
+    st.write("## 1. Comparaison de différents type de modèles bruts")
     st.dataframe(df1)
 
+    # Partie 2    
+    st.write("## 2. Feature Selection")
+    st.write('''
+    Afin d'optimiser l'entrainement de notre modèle, nous devons choisir judicieusement les features à considérer.
+    ''')
+    st.write('''Il existe différentes méthode pour selectionner les meilleures features de notre modèle:
+    - Une sélection à la main des features qui nous semblent les plus importantes (par exemple à partir de notre exercice de datavizualisation). Cependant au vu de notre niveau d'expertise sur le sujet des accidents, ainsi qu'au vu du nombre de variables et des interactions, cela ne nous semblait pas pertinent. 
+    - Une sélection ou réduction des features via une matrice de corrélation en regardant les corrélation entre les variables.
+    - Une sélection des features en utilisant des modèles avancés déjà implémentés sur Python (SelectKBest / SelectFromModel)
+    ''')
+    
+    st.image("img/skb_vs_sfm.png.png", caption="Comparaison sfm vs skb", use_column_width=True)
+
+    st.write("### 2.A. Méthode Corrélation")
+    st.write("On remarque que nous avons un grand nombre de variables. Mais nous n'observons pas particulièrement de corrélations fortes (-1 ou 1). Les seules fortes corrélations que nous observons se font entre les variables qui recensent les piétons. Nous décidons de ne pas prendre de décision à ce stade.")
+
+    st.write("### 2.B. Méthode SelectKbest")
+    st.write("Nous avons appliqué une features selection avec un selectKbest qui se base sur une analyse univariée des features. Cette méthode est indépendante du modèle selectionné. Afin de faire plusieurs test, on se base sur une sélection de 10 seuils qui vont sélectionner un nombre de feature compris 10 et 100. Puis on lance un modèle avec chaque sélection de features afin de trouver le set de features qui parvient à obtenir la meilleure accuracy.")
+    st.image("img/skb.png", caption="Features Selection sfm Lightgbm", use_column_width=True)
+    st.write("On remarque que le nombre de features optimal est 60 avec cette méthode, on garde donc ce set de features.")
+
+    st.write("### 2.C. Méthode 2 : SelectFromModel")
+    st.write("Nous avons appliqué une features selection avec un SelectFromModel. Pour cette librairie, le modèle selectionné à une importance sur la sélection de features. On le teste donc avec nos 3 modèles brutes. L'idée est de donner une grille de seuil à tester, et sélectionner le set de features qui parvient à obternir les meilleures performances")
+    st.image("img/lightgbm_sfm.png", caption="Features Selection sfm Lightgbm", use_column_width=True)
+    st.image("img/xgboost_sfm.png", caption="Features Selection sfm Lightgbm", use_column_width=True)
+    st.image("img/rf_sfm.png", caption="Features Selection sfm Lightgbm", use_column_width=True)
+    st.write("On remarque que pour le xgboost et le lightgbm, l'accuracy décroit lorsque le seuil augmente. Nous n'avons donc pas été capable de trouver un set de features convenable. Cependant, pour le Random Forest, nous avons trouvé un seuil qui sélectionne un set de variable qui ont de bonnes performances.")
+    st.write('''En conclusion de cette partie Feature Selection, nous décidons de sélectionner 3 set de features:
+             - Les 60 features du SelectKBest.
+             - Le seuil (0.01) du SelectFromModel du Random Forest.
+             - Le seuil (0.00925) du SelectFromModel du Random Forest.
+             ''')
+    
+    st.code('''
+    # Assignation des ensembles de features sélectionnées
+    X_k60 = X['age', 'place_1.0', 'place_2.0', 'catu_2', 'catu_3', 'sexe_2', 'trajet_2.0', 'trajet_4.0', 'trajet_5.0', 'locp_0.0', 'locp_1.0', 'locp_2.0', 'locp_3.0', 'locp_4.0', 'actp_0.0', 'actp_1.0', 'actp_3.0', 'etatp_0.0', 'etatp_1.0', 'etatp_2.0', 'senc_1.0', 'catv_33', 'obs_0.0', 'obs_13.0', 'obs_2.0', 'obs_6.0', 'obs_8.0', 'obsm_0.0', 'obsm_2.0', 'choc_4.0', 'manv_13.0', 'manv_2.0', 'manv_23.0', 'catr_3.0', 'catr_4.0', 'circ_1.0', 'circ_2.0', 'prof_0.0', 'prof_1.0', 'plan_1.0', 'plan_2.0', 'plan_3.0', 'situ_1.0', 'situ_3.0', 'lum_3', 'agg_2', 'int_1', 'int_3', 'col_2.0', 'col_3.0', 'col_4.0', 'col_5.0', 'col_6.0', 'col_7.0', 'com_55', 'dep_130', 'dep_750', 'dep_910', 'dep_920', 'dep_940']
+    X_sfm_1 = ['lartpc', 'larrout', 'mois', 'jour', 'hrmn', 'age', 'place_1.0', 'sexe_2', 'trajet_0.0', 'trajet_1.0', 'trajet_4.0', 'trajet_5.0', 'choc_1.0', 'manv_1.0', 'catr_3.0', 'agg_2']
+    X_sfm_2 = ['lartpc', 'larrout', 'mois', 'jour', 'hrmn', 'age', 'place_1.0', 'sexe_2', 'trajet_0.0', 'trajet_4.0', 'trajet_5.0', 'catr_3.0', 'agg_2']
+    ''', language='python')
+    
+    # Partie 3
     # Deuxième tableau : Comparaison des modèles avec sélection de caractéristiques
     data2 = {
         'Modèle': [
@@ -92,7 +132,7 @@ def app():
             'Random forest avec Features selection "SKB" - 60 features',
             'Random forest avec Features selection "SFM" - seuil (0.01)',
             'Random forest avec Features selection "SFM" - seuil (0.00925)',
-            'Random forest avec Features selection "SFM" - seuil (0.00925) - meilleurs paramètres'
+            'Random forest avec Features selection "SFM" - seuil (0.00925) - meilleurs paramètres (GridSearch)'
         ],
         'Accuracy': [0.60, 0.68, 0.72, 0.846, 0.849],
         'Recall': [0.61, 0.67, 0.71, 0.835, 0.838],
@@ -102,5 +142,6 @@ def app():
     df2 = pd.DataFrame(data2)
     df2 = df2.set_index('Modèle')
 
-    st.write("## Comparaison des différentes phases d'optimisation du Random Forest")
+    st.write("## 3. Comparaison des différentes phases d'optimisation du Random Forest")
+    st.write("Dans cette partie, nous avons testé de nombreux modèles différents (xgboost, lightgbm, random forest). Cependant nous avons décidé de ne présenter que le Random Forest qui est le modèle le plus performant pour notre problèmatique. A l'aide d'optimisation, voici l'évolution des performances que nous avons réussi à atteindre:")
     st.dataframe(df2)
