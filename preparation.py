eb0cf9fcb64613b1356c2ead28a59548ae7943e2
AYEVA98 <162471963+AYEVA98@users.noreply.github.com>
2024-10-14 07:38:01 +0200

    Update preparation.py

--- preparation.py
+++ preparation.py
@@ -8,3 +8,13 @@ from streamlit_option_menu import option_menu
 
 def app():
     st.title("Préparation des Données")
+
+     # Chart 1
+    st.image("10.png")
+
+     # Chart 2
+    st.image("11.png")  
+
+     # Chart 3
+    st.image("12.png")
+    
