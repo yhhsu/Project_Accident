62430eac1929a36f269d19faec45404d78147169
cedrickhonekhammy <41993081+cedrickhonekhammy@users.noreply.github.com>
2024-10-14 10:19:06 +0200

    Update interpretation.py

--- interpretation.py
+++ interpretation.py
@@ -8,26 +8,22 @@ from streamlit_option_menu import option_menu
 
 def app():
     st.title("Interprétation")
-    st.write('''
-    Dans un premier temps, nos taux de précision n’étaient pas idéaux. Nous avions pour 
-    3 modéles pré-entrainés des taux de précision autour de 60-70% ce qui reste encore 
-    à  améliorer  surtout  que  les  classes  minoritaires  avaient  des  taux  de  précision  très 
-    faibles.
-    ''')
-    st.write("Par la suite, avec les amélioration apportées,")
-    st.write("### XGBoost")
-    st.image("xgboost.png")
-    st.write("### LightGBM")
-    st.image("lightgbm.png")
-    st.write("### SVM")
-    st.image("svm.png")
-    st.write("### Random Forest")
-    st.image("random_forest.png")
-    st.write('''
-    Nous convenons donc ensemble que le modèle le plus optimisé dans notre 
-    problématique  de  prédiction  de  la  gravité  des  accidents  est  le  Random  Forest  car 
-    sur  l’intégralité  de  ses  métriques  surtout  l’Accuracy,  il  surpasse  de  loin  les  autres 
-    modèles.
-    ''')
-    st.write("C’est  donc  le  Random  Forest  qui  est  retenu  comme  modèle  le  plus  optimal  après différentes itérations et différents tests.")
+        # Partie 1
+        st.write("## 1. Feature Importance")
+
+        # Partie 2
+        st.write("## 2. Interprétation d'une prédiction")
+
+        # Partie 2.A
+        st.write("### 2.A. LIME")
+
+        # Partie 2.B
+        st.write("### 2.B. Interprétation prédiction de la classe "Etat indemne")
+
+        # Partie 2.C
+        st.write("### 2.C. Interprétation prédiction de la classe "Etat blessures légères")
+
+        # Partie 2.D
+        st.write("### 2.D. Interprétation prédiction de la classe "Etat grave")
+
     
