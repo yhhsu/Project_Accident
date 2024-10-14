b5333f2d9da2ca5f758d23881301768909396a2b
cedrickhonekhammy <41993081+cedrickhonekhammy@users.noreply.github.com>
2024-10-14 10:21:52 +0200

    Update interpretation.py

--- interpretation.py
+++ interpretation.py
@@ -8,22 +8,22 @@ from streamlit_option_menu import option_menu
 
 def app():
     st.title("Interprétation")
-        # Partie 1
-        st.write("## 1. Feature Importance")
+    # Partie 1
+    st.write("## 1. Feature Importance")
 
-        # Partie 2
-        st.write("## 2. Interprétation d'une prédiction")
+    # Partie 2
+    st.write("## 2. Interprétation d'une prédiction")
 
-        # Partie 2.A
-        st.write("### 2.A. LIME")
+    # Partie 2.A
+    st.write("### 2.A. LIME")
 
-        # Partie 2.B
-        st.write("### 2.B. Interprétation prédiction de la classe Etat indemne")
+    # Partie 2.B
+    st.write("### 2.B. Interprétation prédiction de la classe Etat indemne")
 
-        # Partie 2.C
-        st.write("### 2.C. Interprétation prédiction de la classe Etat blessures légères")
+    # Partie 2.C
+    st.write("### 2.C. Interprétation prédiction de la classe Etat blessures légères")
 
-        # Partie 2.D
-        st.write("### 2.D. Interprétation prédiction de la classe Etat grave")
+    # Partie 2.D
+    st.write("### 2.D. Interprétation prédiction de la classe Etat grave")
 
     
